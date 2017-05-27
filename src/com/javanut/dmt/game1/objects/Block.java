a6ac97b85adbe5186f193ec2a4e2c11221ca74ad
DavidTippy <DavidTippy@DESKTOP-S9AI5U8.lan>
2017-05-27 16:00:36 -0500

    Added Block class

--- src/com/javanut/dmt/game1/objects/Block.java
+++ src/com/javanut/dmt/game1/objects/Block.java
@@ -1,7 +1,22 @@
 package com.javanut.dmt.game1.objects;
 
-public class Block {
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Block extends Rectangle{
 	
+	public static final int blockSize = 32;
 	
+	public Block(int x, int y) {
+		
+		setBounds(x,y,blockSize,blockSize);
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+		g.fillRect(x, y, width, height);
+		
+	}
 
 }
